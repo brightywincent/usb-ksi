7cffa55eccddf362a4f93d897ffe729a18ec8e13
Brighty Wincent <brightywincent77@gmail.com>
2025-10-02 14:20:13 +0530

    debugged and finalized oversampler.v , Simulated it using a dedicated temporary testbench

--- USB_PHY/oversampler.v
+++ USB_PHY/oversampler.v
@@ -7,16 +7,39 @@ module oversampler(
 	output reg dm
 );
 	reg [2:0]counter;
+	reg counter_en;
+	reg dp_pa,dm_pa;
+	
+	initial begin
+		counter = 3'd0;
+		counter_en = 1'b0;
+		dp = 1'b0;
+		dm = 1'b0;
+	end
+	
 	always@(posedge clk_8x or posedge reset) begin
 		if(reset) begin
 			counter<=3'd0;
+			counter_en <= 1'b0;
+			dp <= 1'b0;
+			dm <= 1'b0;
+			dp_pa <= 1'b0;
+			dm_pa <= 1'b0;
 		end
 		else begin
-			counter<=(counter==3'd7)?3'd0:counter+1'b1;
-			if(counter==3'd4) begin
-				dp<=d_plus_device;
-				dm<=d_minus_device;
+			if(dp_pa != d_plus_device || dm_pa != d_minus_device) begin
+				counter_en <= 1'b1;
+				counter <= 3'd1;
+			end
+		end
+		dp_pa <= d_plus_device;
+		dm_pa <= d_minus_device;
+		if(counter_en) begin
+			counter <= (counter==3'd7)?3'd0:counter+1;
+			if(counter == 3'd4) begin
+				dp <= d_plus_device;
+				dm <= d_minus_device;
 			end
 		end
 	end
-	endmodule
+endmodule
