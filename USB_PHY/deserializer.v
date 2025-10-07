32ca242d968e3c44f86ef769dab1bdc6ade96095
Brighty Wincent <brightywincent77@gmail.com>
2025-10-07 20:00:59 +0530

    optimized deserializer.v but not yet simulated.

--- USB_PHY/deserializer.v
+++ USB_PHY/deserializer.v
@@ -26,56 +26,19 @@ module deserializer(
 		else begin
 			if(!unstuff) begin
 				case(byte) 
-					2'b00 : begin
-						if(count<4'd8) begin
-							data_32[7:0] <= {b,data_32[7:1]};
-							count <= count+1;
-						end
-						else begin
-							count <= 4'd0;
-							byte <= 2'b01;
-						end
-					end	
-					2'b01 : begin
-						if(count<4'd8) begin
-							data_32[15:8] <= {b,data_32[15:9]};
-							count <= count+1;
-						end
-						else begin
-							count <= 4'd0;
-							byte <= 2'b10;
-						end
-					end
-					2'b10 : begin
-						if(count<4'd8) begin
-							data_32[23:16] <= {b,data_32[23:17]};
-							count <= count+1;
-						end
-						else begin
-							count <= 4'd0;
-							byte <= 2'b11;
-						end
-					end
-					2'b11 : begin
-						if(count<4'd8) begin
-							data_32[31:24] <= {b,data_32[31:25]};
-							count <= count+1;
-						end
-						else begin
-							count <= 4'd0;
-							byte <= 2'b00;
-						end
-					end
-					default : begin
-						byte <= 2'b00;
-						count <= 4'd0;
-					end
+				2'b00 : data_32[7:0] <= {b,data_32[7:1]};
+				2'b01 : data_32[15:8] <= {b,data_32[15:9]};
+				2'b10 : data_32[23:16] <= {b,data_32[23:17]};
+				2'b11 : data_32[31:24] <= {b,data_32[31:25]};
 				endcase
 				if(count==4'd7) begin
-					byte_ready[0] <= (byte==2'b00)?1'b1:1'b0;
-					byte_ready[1] <= (byte==2'b01)?1'b1:1'b0;
-					byte_ready[2] <= (byte==2'b10)?1'b1:1'b0;
-					byte_ready[3] <= (byte==2'b11)?1'b1:1'b0;
+					byte_ready[byte] <= 1'b1;
+					byte <= byte+1;
+					count <= 4'd0;
+				end
+				else begin
+					count <= count+1;
+					byte_ready <= 4'b0000;
 				end
 			end
 		end
