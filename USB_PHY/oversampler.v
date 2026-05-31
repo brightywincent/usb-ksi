53919f34e073a02045dcf908102570487f4dbc05
Brighty Wincent <brightywincent77@gmail.com>
2026-05-31 07:28:32 +0000

    restricted functionality to only oversampling. Removed counter logic that keeps track of one original bit duration.

--- USB_PHY/oversampler.v
+++ USB_PHY/oversampler.v
@@ -1,45 +1,47 @@
 module oversampler(
-	input clk_8x,
-	input d_plus_device,
-	input d_minus_device,
+	input clk_8x_i,
+	input dp_i,
+	input dm_i,
 	input reset,
-	output reg dp,
-	output reg dm
+	output reg dp_o,
+	output reg dm_o
 );
-	reg [2:0]counter;
+	/*reg [2:0]counter;
 	reg counter_en;
 	reg dp_pa,dm_pa;
 	
 	initial begin
 		counter = 3'd0;
 		counter_en = 1'b0;
-		dp = 1'b0;
-		dm = 1'b0;
-	end
+		dp_o = 1'b0;
+		dm_o = 1'b0;
+	end*/
 	
-	always@(posedge clk_8x or posedge reset) begin
+	always@(posedge clk_8x_i or posedge reset) begin
 		if(reset) begin
-			counter<=3'd0;
-			counter_en <= 1'b0;
-			dp <= 1'b0;
-			dm <= 1'b0;
-			dp_pa <= 1'b0;
-			dm_pa <= 1'b0;
+			//counter<=3'd0;
+			//counter_en <= 1'b0;
+			dp_o <= 1'b0;
+			dm_o <= 1'b0;
+			//dp_pa <= 1'b0;
+			//dm_pa <= 1'b0;
 		end
 		else begin
-			if(dp_pa != d_plus_device || dm_pa != d_minus_device) begin
+			/*if(dp_pa != dp_i || dm_pa != dm_i) begin
 				counter_en <= 1'b1;
 				counter <= 3'd1;
 			end
 		end
-		dp_pa <= d_plus_device;
-		dm_pa <= d_minus_device;
+		dp_pa <= dp_i;
+		dm_pa <= dm_i;
 		if(counter_en) begin
 			counter <= (counter==3'd7)?3'd0:counter+1;
 			if(counter == 3'd4) begin
-				dp <= d_plus_device;
-				dm <= d_minus_device;
+				dp_o <= dp_i;
+				dm_o <= dm_i;
 			end
-		end
+		end*/
+		dp_o<=dp_i;
+		dm_o<=dm_i;
 	end
 endmodule
