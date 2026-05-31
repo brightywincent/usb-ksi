bacdbb31aeea9576a006353dac0b7cdca5a2e8d9
Brighty Wincent <brightywincent77@gmail.com>
2026-05-31 18:04:37 +0000

    corrected syntax error/s , compiled , simulated and generated waveforms.

--- USB_PHY/oversampler.v
+++ USB_PHY/oversampler.v
@@ -6,42 +6,16 @@ module oversampler(
 	output reg dp_o,
 	output reg dm_o
 );
-	/*reg [2:0]counter;
-	reg counter_en;
-	reg dp_pa,dm_pa;
-	
-	initial begin
-		counter = 3'd0;
-		counter_en = 1'b0;
-		dp_o = 1'b0;
-		dm_o = 1'b0;
-	end*/
 	
 	always@(posedge clk_8x_i or posedge reset) begin
 		if(reset) begin
-			//counter<=3'd0;
-			//counter_en <= 1'b0;
 			dp_o <= 1'b0;
 			dm_o <= 1'b0;
-			//dp_pa <= 1'b0;
-			//dm_pa <= 1'b0;
 		end
 		else begin
-			/*if(dp_pa != dp_i || dm_pa != dm_i) begin
-				counter_en <= 1'b1;
-				counter <= 3'd1;
-			end
+			dp_o<=dp_i;
+			dm_o<=dm_i;
 		end
-		dp_pa <= dp_i;
-		dm_pa <= dm_i;
-		if(counter_en) begin
-			counter <= (counter==3'd7)?3'd0:counter+1;
-			if(counter == 3'd4) begin
-				dp_o <= dp_i;
-				dm_o <= dm_i;
-			end
-		end*/
-		dp_o<=dp_i;
-		dm_o<=dm_i;
 	end
+	
 endmodule
