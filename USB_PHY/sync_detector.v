231c79a70952db2ce05fb18ef5069217598d3308
Brighty Wincent <brightywincent77@gmail.com>
2026-06-03 11:21:00 +0000

    sync_detector module is almost done. few cycles of latency is yet to be cleared

--- USB_PHY/sync_detector.v
+++ USB_PHY/sync_detector.v
@@ -4,37 +4,7 @@ module sync_detector(
 	input wire sd_reset,
 	output reg sd_sync_o
 );
-	/*reg [3:0]sd_count;
-	reg [1:0]sd_prev;
-	always@(posedge sd_clk_i or posedge sd_reset)begin
-		if(sd_reset)begin
-			sd_sync_o<=1'b0;
-			sd_prev<=2'b01;
-			sd_count<={4{0}};
-		end
-		else begin
-			sd_prev<=sd_sbus_i;
-			if(sd_count<15)begin
-				sd_sync_o<=1'b0;
-				if((sd_prev==2'b01 &&sd_sbus_i==2'b10) || (sd_prev==2'b10 &&sd_sbus_i==2'b01))begin
-					sd_count<=sd_count+1;
-				end
-				else sd_count<={4{0}};
-			end
-			else if(sd_count==15)begin
-				if(sd_prev==sd_sbus_i)begin
-					sd_sync_o<=1'b1;
-					sd_count<={4{0}};
-				
-				else begin
-					sd_count<={4{0}};
-					sd_sync_o<=1'b0;
-				end
-			end
-		end
-	end
-endmodule
-*/
+	
 	localparam IDLE=2'b00;
 	localparam ACQUIRING=2'b01;
 	localparam SYNC_FOUND=2'b10;
@@ -43,11 +13,11 @@ endmodule
 	reg [3:0]sd_count;
 	reg [1:0]sd_prev;
 
-	always@(posedge sd_clock_i or posedge sd_reset)begin
+	always@(posedge sd_clk_i or posedge sd_reset)begin
 		if(sd_reset)begin
 			sd_sync_o<=1'b0;
 			sd_prev<=2'b01;
-			sd_count<={4{0}};
+			sd_count<={4{1'b0}};
 			STATE<=IDLE;
 		end
 		else begin
@@ -55,7 +25,7 @@ endmodule
 			case(STATE)
 				IDLE : begin
 					sd_sync_o<=1'b0;
-					sd_count<={4{0}};
+					sd_count<={4{1'b0}};
 					if(sd_sbus_i==2'b10)begin
 						STATE<=ACQUIRING;
 					end
@@ -80,3 +50,4 @@ endmodule
 			endcase
 		end
 	end
+endmodule
