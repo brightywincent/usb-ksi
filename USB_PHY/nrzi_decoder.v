fbe23939cc2c8c9895792c5840c34e55678bf753
Brighty Wincent <brightywincent77@gmail.com>
2026-06-04 05:53:49 +0000

    Renamed the datatypes, removed initial block in the nrzi_decoder module. Not yet simulated.

--- USB_PHY/nrzi_decoder.v
+++ USB_PHY/nrzi_decoder.v
@@ -1,34 +1,28 @@
 module nrzi_decoder(
-	input wire clk,
-	input wire dp,
-	input wire dm,
-	input wire reset,
-	output reg bit
+	input wire nd_clk_i,
+	input wire nd_dplus_i,
+	input wire nd_dminus_i,
+	input wire nd_reset,
+	output reg nd_sbit_o
 );
-	reg dpp,dmp;
+	reg nd_dplus_prev,nd_dminus_prev;
 	
-	initial begin
-		dpp = 1'b0;
-		bit = 1'b0;
-		dmp = 1'b0;
-	end
-	
-	always@(posedge clk or posedge reset)begin
-		if(reset) begin
-			dpp <= 1'b0;
-			dmp <= 1'b0;
-			bit <= 1'b0;
+	always@(posedge nd_clk_i or posedge nd_reset)begin
+		if(nd_reset) begin
+			nd_dplus_prev <= 1'b0;
+			nd_dminus_prev <= 1'b0;
+			nd_sbit_o <= 1'b0;
 		end
 		else begin
-			dpp <= dp;
-			dmp <= dm;
-			if(dp==~dpp || dm==~dmp) begin
-				bit <= 1'b0;
+			nd_dplus_prev <= nd_dplus_i;
+			nd_dminus_prev <= nd_dminus_i;
+			if(nd_dplus_i==~nd_dplus_prev || nd_dminus_i==~nd_dminus_prev) begin
+				nd_sbit_o <= 1'b0;
 			end
 			else begin
-				bit <= 1'b1;
+				nd_sbit_o <= 1'b1;
 			end
 		end
 	end
-endmodule
+ennd_dminus_iodule
 
