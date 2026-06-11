a38aa0d2b325b5dac3f88a7365357a606e413477
Brighty Wincent <brightywincent77@gmail.com>
2026-06-11 07:52:58 +0000

    PID Validator : Renamed datatypes in the module from pd to pv

--- SIE/pid_validator.v
+++ SIE/pid_validator.v
@@ -1,26 +1,26 @@
-module pid_decoder(
-	input wire pd_clk_i,
-	input wire pd_reset, 
-	input wire [7:0]pd_pbyte0_i,
-	input wire pd_pbyte0_ready_i, 
-	output reg pd_valid_o,
-	output reg pd_error_o
+module pid_validator(
+	input wire pv_clk_i,
+	input wire pv_reset, 
+	input wire [7:0]pv_pbyte0_i,
+	input wire pv_pbyte0_ready_i, 
+	output reg pv_valid_o,
+	output reg pv_error_o
 );
 	
-	always@(posedge pd_clk_i or posedge pd_reset) begin
-		if(pd_reset) begin
-			pd_valid_o <= 1'b0;
-			pd_error_o<=1'b0;
+	always@(posedge pv_clk_i or posedge pv_reset) begin
+		if(pv_reset) begin
+			pv_valid_o <= 1'b0;
+			pv_error_o<=1'b0;
 		end
 		else begin
-			pd_valid_o<=1'b0;
-			pd_error_o<=1'b0;
-			if(pd_pbyte0_ready_i)begin
-				if(pd_pbyte0_i[7:4]==~pd_pbyte0_i[3:0])begin
-					pd_valid_o<=1'b1;
+			pv_valid_o<=1'b0;
+			pv_error_o<=1'b0;
+			if(pv_pbyte0_ready_i)begin
+				if(pv_pbyte0_i[7:4]==~pv_pbyte0_i[3:0])begin
+					pv_valid_o<=1'b1;
 				end
 				else begin
-					pd_error_o<=1'b1;
+					pv_error_o<=1'b1;
 				end
 			end
 		end
