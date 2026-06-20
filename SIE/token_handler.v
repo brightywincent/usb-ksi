5314a8b318cc3ac787d792c75cfc97d76c67c955
Brighty Wincent <brightywincent77@gmail.com>
2026-06-20 20:01:50 +0000

    Token Handler : renamed th_enable as th_trigger. Added a 1-bit register that stores endp[0] which arrives in the byte - {endp[0],addr} when byte_ready is enabled. Changed logic in IDLE state such that addr and endp are not cleared until the arrival of next token packet.

--- SIE/token_handler.v
+++ SIE/token_handler.v
@@ -1,7 +1,7 @@
 module token_handler(
     input wire th_clk_i,
     input wire th_reset,
-    input wire th_enable,
+    input wire th_trigger,
     input wire th_eop_i,
     input wire th_sync_i,
     input wire [10:0]th_bytes_i,  //only the 11 bits : 7 addr + 4 endp
@@ -19,6 +19,7 @@ module token_handler(
     localparam ACTIVE = 2'b01;
 
     reg [1:0]STATE;
+    reg th_byte1;
     
     always@(posedge th_clk_i or posedge th_reset)begin
         if(th_reset)begin
@@ -37,24 +38,28 @@ module token_handler(
             th_packet_error_o<=1'b0;
             case(STATE)
                 IDLE : begin
-                    STATE<=(th_enable)?ACTIVE:IDLE;
-                    th_addr_o<=7'b0;
-                    th_endp_o<=4'b0;
+                    if(th_trigger)begin
+                        th_addr_o<=7'b0;
+                        th_endp_o<=4'b0;
+                        STATE<=ACTIVE;
+                    end
                 end
                 ACTIVE : begin
-                    if(th_byte_ready_i[0])begin
-                        th_addr_o<=th_bytes_i[10:4];
+                    if(th_byte_ready_i[0]==1'b1)begin
+                        th_byte1<=th_bytes_i[7];
+                        th_addr_o<=th_bytes_i[6:0];
                     end
-                    if(th_byte_ready_i[1])begin
-                        th_endp_o<=th_bytes_i[3:0];
-                        STATE<=IDLE;
+                    if(th_byte_ready_i[1]==1'b1)begin
+                        th_endp_o<={th_bytes_i[10:8],th_byte1};
                         if(th_crc5_i==5'b0)begin
                             th_token_valid_o<=1'b1;
                             th_crc_reset_o<=1'b1;
+                            STATE<=IDLE;
                         end
                         else begin
                             th_crc_fail_o<=1'b1;
                             th_crc_reset_o<=1'b1;
+                            STATE<=IDLE;
                         end   
                     end
                 end
