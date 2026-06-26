b172fa71a29ce0157e9f20984ebc13857743ca57
Brighty Wincent <brightywincent77@gmail.com>
2026-06-26 16:09:01 +0000

    RX FIFO : Some minor fixes. Finally ready to be tested.

--- SIE/rx_fifo.v
+++ SIE/rx_fifo.v
@@ -1,14 +1,14 @@
 module rx_fifo(
-     input wire rf_clk_i,
-     input wire rf_reset,
-     input wire [7:0]rf_byte_i,
-     input wire rf_write_i,
-     input wire rf_read_i,
-     output reg [7:0]rf_byte_o,
-     output reg rf_empty_o,
-     output reg rf_full_o,
-     output reg rf_underflow_o,
-     output reg rf_overflow_o,
+    input wire rf_clk_i,
+    input wire rf_reset,
+    input wire [7:0]rf_byte_i,
+    input wire rf_write_i,
+    input wire rf_read_i,
+    output reg [7:0]rf_byte_o,
+    output reg rf_empty_o, //flag
+    output reg rf_full_o,  //flag
+    output reg rf_underflow_o, //pulse
+    output reg rf_overflow_o //pulse
 );
     reg [7:0] fifo_mem[63:0];
     reg [5:0]rd_ptr;
@@ -26,30 +26,41 @@ module rx_fifo(
             byte_count<=7'b0;
         end
         else begin
-            case({rf_write_i && (byte_count!=7'd64),rf_read_i && (byte_count!=7'd0)})
+            rf_overflow_o<=1'b0;
+            rf_underflow_o<=1'b0;
+            case({rf_write_i,rf_read_i})
+                2'b00 : begin
+                    rf_byte_o<=8'bx;
+                end
                 2'b10 : begin
-                    fifo_mem[wr_ptr]<=rf_byte_i;
-                    wr_ptr<=wr_ptr+1;
-                    byte_count<=byte_count+1;
+                    if(byte_count!=7'd64)begin
+                        fifo_mem[wr_ptr]<=rf_byte_i;
+                        wr_ptr<=wr_ptr+1;
+                        byte_count<=byte_count+1;
+                    end
+                    else 
+                        rf_overflow_o<=1'b1;
+                        rf_byte_o<=8'bx;
                 end
                 2'b01 : begin
-                    rf_byte_o<=fifo_mem[rd_ptr];
-                    rd_ptr<=rd_ptr+1;
-                    byte_count<=byte_count-1;
+                    if(byte_count!=7'd0)begin
+                        rf_byte_o<=fifo_mem[rd_ptr];
+                        rd_ptr<=rd_ptr+1;
+                        byte_count<=byte_count-1;
+                    end
+                    else
+                        rf_underflow_o<=1'b1;
+                        rf_byte_o<=8'bx;
                 end
                 2'b11 : begin
-                    fifo_mem[wr_ptr]<=rf_byte_i;
-                    wr_ptr<=wr_ptr+1;
-                    rf_byte_o<=fifo_mem[rd_ptr];
-                    rd_ptr<=rd_ptr+1;
+                    rf_byte_o<=rf_byte_i;
                 end
                 default : begin
+                    rf_byte_o<=8'bx;
                 end
             endcase
             rf_empty_o<=(byte_count==7'b0);
-            rf_underflow_o<=((byte_count==7'b0) && rf_read_i);
             rf_full_o<=(byte_count==7'd64);
-            rf_overflow_o<=((byte_count==7'd64) && rf_write_i);
         end
     end
 
