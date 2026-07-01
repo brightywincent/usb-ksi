b79ecdfce586eaf48e8aad82be7b1dccfcc1c2d6
Brighty Wincent <brightywincent77@gmail.com>
2026-07-01 20:12:13 +0000

    Transaction Engine : OUT transaction is handled. Still, there are other cases to be handled.

--- SIE/transaction_engine.v
+++ SIE/transaction_engine.v
@@ -20,6 +20,8 @@ module transaction_engine(
     input wire te_data_pac_err_i;
     input wire te_crc16_fail_i;
 
+    input wire te_handshake_trigger_i;
+
     input wire te_timeout_i;
 
     output reg te_send_ack_o;
@@ -27,12 +29,14 @@ module transaction_engine(
     output reg te_protocol_err_o;
 );
 
-    localparam IDLE=2'b00;
-    localparam TOKEN=2'b01;
-    localparam DATA=2'b10;
-    localparam HANDSHAKE=2'b11;
+    localparam IDLE=3'b000;
+    localparam OUT_TR=3'b001;
+    localparam IN_TR=3'b010;
+    localparam =3'b011;
 
-    reg [1:0]STATE;
+
+    reg [2:0]STATE;
+    reg [2:0]SUB_STATE;
 
     reg te_ignore; //just for testing
     reg te_abort;
@@ -41,35 +45,85 @@ module transaction_engine(
         if(te_reset)begin
             te_send_ack_o<=1'b0;
             te_protocol_err_o<=1'b0;
-            STATE<=2'b0;
+            te_ignore<=1'b0;
+            te_abort<=1'b0;
+            STATE<=3'b0;
+            SUB_STATE<=3'b0;
         end
         else begin
+            te_ignore<=1'b0;
+            te_abort<=1'b0;
+            te_protocol_err_o<=1'b0;
+            te_send_ack_o<=1'b0;
             case(STATE)
                 IDLE : begin
                     if(te_token_trigger_i)begin
-                        STATE<=TOKEN;
+                        if(te_pid_nibble_i==`PIDN_OUT)begin
+                            STATE<=OUT_TR;
+                        end
+                        if(te_pid_nibble_i==`PIDN_IN)begin
+                            STATE<=IN_TR;
+                        end
                     end
                 end
-                TOKEN : begin
-                    case(te_pid_nibble_i)
-                        `PIDN_OUT : begin
-                            if(te_token_valid_i)begin
-                                STATE<=DATA;
+                OUT_TR : begin
+                    case(SUB_STATE)
+                        IDLE : begin
+                            SUB_STATE<=IDLE;
+                            if(te_token_pac_err_i || te_crc5_fail_i)begin
+                                te_ignore<=1'b1;
+                                STATE<=IDLE;
+                                SUB_STATE<=IDLE;
+                            end
+                            else if(te_token_valid)begin
+                                SUB_STATE<=OUT;
+                                //note address and endpoint
                             end
                         end
-                        `PIDN_IN : begin
-                            
+                        OUT : begin
+                            if(te_timeout_i)begin
+                                te_abort<=1'b1;
+                                SUB_STATE<=IDLE;
+                                STATE<=IDLE;
+                            end
+                            else if(te_handshake_trigger_i)begin
+                                te_protocol_err_o<=1'b1;
+                                SUB_STATE<=IDLE;
+                                STATE<=IDLE;
+                            end
+                            else if(te_token_trigger_i)begin
+                                te_abort<=1'b1;
+                                SUB_STATE<=IDLE;
+                                STATE<=IDLE;
+                            end
+                            else if(te_data_trigger_i)begin
+                                SUB_STATE<=DATA;
+                            end
                         end
-                        `PIDN_SETUP :begin
-
+                        DATA : begin
+                            if(te_timeout_i)begin
+                                te_abort<=1'b1;
+                                SUB_STATE<=IDLE;
+                                STATE<=IDLE;
+                            end
+                            else if(te_data_pack_err_i || te_crc16_fail_i)begin
+                                te_ignore<=1'b1;
+                                SUB_STATE<=IDLE;
+                                STATE<=IDLE;
+                            end
+                            else if(te_data_valid)begin
+                                te_send_ack_o<=1'b1;
+                                SUB_STATE<=IDLE;
+                                STATE<=IDLE;
+                            end
                         end
+                        default : SUB_STATE<=IDLE;
                     endcase
-                end
-                DATA : begin
-
-                end
-                HANDSHAKE : begin
-
+                    IN_TR : begin
+                        case()
+                            
+                        endcase
+                    end
                 end
                 default : STATE<=IDLE;
             endcase
