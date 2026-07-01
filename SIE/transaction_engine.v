c1f39f76f1a2f77e1ec5b25a188219b4fcc07718
Brighty Wincent <brightywincent77@gmail.com>
2026-07-01 20:51:42 +0000

    Transaction Engine : Completed the IN transaction part. Still, there are cases to be handled.

--- SIE/transaction_engine.v
+++ SIE/transaction_engine.v
@@ -1,32 +1,46 @@
 `include "PID.vh"
-module transaction_engine(
-    input wire te_clk_i;
-    input wire te_reset;
-    input wire te_sync_i;
-    input wire te_eop_i;
+module transaction_engine#(
+    parameter RESPONSE_TIMEOUT=10'd816;
+)(
+    input wire te_clk_i,
+    input wire te_reset,
+    input wire te_sync_i,
+    input wire te_eop_i,
 
-    input wire [3:0]te_pid_nibble_i;
-    input wire te_pid_valid_i;
+    input wire [3:0]te_pid_nibble_i,
+    input wire te_pid_valid_i,
 
-    input wire te_token_trigger_i;
-    input wire te_token_valid_i;
-    input wire te_token_pac_err_i;
-    input wire te_crc5_fail_i;
-    input wire [6:0]te_addr_i;
-    input wire [3:0]te_endp_i;
+    input wire te_token_trigger_i,
+    input wire te_token_valid_i,
+    input wire te_token_pac_err_i,
+    input wire te_crc5_fail_i,
+    input wire [6:0]te_addr_i,
+    input wire [3:0]te_endp_i,
 
-    input wire te_data_trigger_i;
-    input wire te_data_valid_i;
-    input wire te_data_pac_err_i;
-    input wire te_crc16_fail_i;
+    input wire te_data_trigger_i,
+    input wire te_data_valid_i,
+    input wire te_data_pac_err_i,
+    input wire te_crc16_fail_i,
 
-    input wire te_handshake_trigger_i;
+    input wire te_handshake_trigger_i,
 
-    input wire te_timeout_i;
+    input wire te_timeout_i,
 
-    output reg te_send_ack_o;
+    input wire te_endp_empty_i,
+    input wire te_endp_stalled_i,
+    input wire te_endp_ready_i,  //flag
+    input wire te_ack_i,
+    input wire te_stall_i,
+    input wire te_nak_i,
 
-    output reg te_protocol_err_o;
+    output reg te_send_ack_o,
+    output reg te_send_nak_o,
+    output reg te_send_stall_o,
+    output reg te_send_data_o,
+    output reg te_data_toggle_o,
+    output reg te_clear_endp_buff_o,
+
+    output reg te_protocol_err_o
 );
 
     localparam IDLE=3'b000;
@@ -38,23 +52,36 @@ module transaction_engine(
     reg [2:0]STATE;
     reg [2:0]SUB_STATE;
 
+    reg[9:0]te_timer;
+
     reg te_ignore; //just for testing
     reg te_abort;
 
     always@(posedge te_clk_i or posedge te_reset)begin
         if(te_reset)begin
             te_send_ack_o<=1'b0;
+            te_send_nak_o<=1'b0;
+            te_send_data_o<=1'b0;
+            te_send_stall_o<=1'b0;
+            te_data_toggle_o<=1'b0;
+            te_clear_endp_buff_o<=1'b0;
             te_protocol_err_o<=1'b0;
             te_ignore<=1'b0;
             te_abort<=1'b0;
             STATE<=3'b0;
             SUB_STATE<=3'b0;
+            te_timer<=10'b0;
         end
         else begin
             te_ignore<=1'b0;
             te_abort<=1'b0;
             te_protocol_err_o<=1'b0;
             te_send_ack_o<=1'b0;
+            te_send_nak_o<=1'b0;
+            te_send_stall_o<=1'b0;
+            te_send_data_o<=1'b0;
+            te_data_toggle_o<=1'b0;
+            te_clear_endp_buff_o<=1'b0;
             case(STATE)
                 IDLE : begin
                     if(te_token_trigger_i)begin
@@ -119,15 +146,62 @@ module transaction_engine(
                         end
                         default : SUB_STATE<=IDLE;
                     endcase
+                end
                     IN_TR : begin
-                        case()
-                            
+                        case(SUB_STATE)
+                            IDLE : begin
+                                SUB_STATE<=IDLE;
+                                if(te_token_pac_err_i || te_crc5_fail_i)begin
+                                    te_ignore<=1'b1;
+                                    STATE<=IDLE;
+                                    SUB_STATE<=IDLE;
+                                end
+                                else if(te_token_valid)begin
+                                    SUB_STATE<=IN;
+                                end 
+                            end
+                            IN : begin
+                                if(te_endp_empty_i)begin
+                                    te_send_nak_o<=1'b1;
+                                    STATE<=IDLE;
+                                    SUB_STATE<=IDLE;
+                                end
+                                else if(te_endp_stalled_i)begin
+                                    te_send_stall_o<=1'b1;
+                                    STATE<=IDLE;
+                                    SUB_STATE<=IDLE;
+                                end
+                                else if(te_endp_ready_i)begin
+                                    te_send_data_o<=1'b1;
+                                    SUB_STATE<=RESPONSE;
+                                end
+                            end
+                            RESPONSE : begin
+                                if(te_ack_i)begin
+                                    te_clear_endp_buff_o<=1'b1;
+                                    te_timer<=10'b0;
+                                    SUB_STATE<=IDLE;
+                                    STATE<=IDLE;
+                                end
+                                else if(te_stall_i || te_nak_i)begin
+                                    te_protocol_err_o<=1'b0;
+                                    te_timer<=10'b0;
+                                    SUB_STATE<=IDLE;
+                                    STATE<=IDLE;
+                                end
+                                else if(te_timer==RESPONSE_TIMEOUT-1)begin
+                                    te_timer<=10'b0;
+                                    SUB_STATE<=IDLE;
+                                    STATE<=IDLE;
+                                end
+                                else 
+                                    te_timer<=te_timer+1;
+                            end
+                            default : SUB_STATE<=IDLE;
                         endcase
                     end
-                end
                 default : STATE<=IDLE;
             endcase
         end
     end
-
 endmodule
