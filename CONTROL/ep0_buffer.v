6074cb33ee4dbe7e90a7ca7eec11ce528dca0a39
Brighty Wincent <brightywincent77@gmail.com>
2026-07-17 17:37:45 +0000

    EP0 Buffer : In progress.

--- CONTROL/ep0_buffer.v
+++ CONTROL/ep0_buffer.v
@@ -1,5 +1,14 @@
 module ep0_buffer(
+    input wire ep0b_clk_i,
+    input wire ep0b_reset,
 
+    input wire ep0b_byte_i
 );
+
+    reg [7:0] ep0_mem[7:0];
+
+    always @(posedge ep0b_clk_i or posedge ep0b_reset)begin
+
+    end
 
 endmodule
