f29c343623951a0c8f544db9d2cdd9cb16a33303
Brighty Wincent <brightywincent77@gmail.com>
2026-07-17 17:36:59 +0000

    Control Engine EP0 : Data path and control path of Control Engine EP0. In progess, subjected to many changes.

--- CONTROL/control_engine_ep0_cp.v
+++ CONTROL/control_engine_ep0_cp.v
@@ -1,5 +1,42 @@
 module control_engine_ep0_cp(
+    input wire cecp_clk_i,
+    input wire cecp_reset,
+
+    input wire cecp_setup_done_i,  //Transaction engine
+    input wire cecp_status_done_i,
+    input wire cecp_timeout_i,
     
+    input wire [7:0]bmRequestType_i, //from datapath
+    input wire [7:0]bRequest_i,
+    input wire [15:0]wValue_i,
+    input wire [15:0]wIndex_i,
+    input wire [15:0]wLength_i,
+
+    output reg [2:0]STATE;
+    output reg descriptor_start_o, //Data path
+    output reg set_address_o,  //Address manager
+    output reg set_configuration_o,  //Config manager
+    output reg start_status_stage_o  //Transaction engine
 );
+
+    localparam IDLE = 3'd0;
+    localparam DECODE = 3'd1;
+
+    always @(posedge cecp_clk_i or posedge cecp_reset)begin
+        if(cecp_reset)begin
+            STATE<=3'b0;
+        end
+        else begin
+            case(STATE)
+                IDLE : begin
+                    if(cecp_setup_done_i)
+                        STATE<=DECODE;
+                end
+                DECODE : begin
+
+                end
+            endcase
+        end
+    end
 
 endmodule
--- CONTROL/control_engine_ep0_dp.v
+++ CONTROL/control_engine_ep0_dp.v
@@ -1,5 +1,33 @@
 module control_engine_ep0_dp(
+    input wire cedp_clk_i,
+    input wire cedp_reset,
+    
+    input wire [63:0]cedp_setup_packet_i, //Transaction engine
+    input wire cedp_out_data_valid_i,
+    input wire [7:0]cedp_out_data_byte_i,
 
+    input wire cedp_descriptor_start_i, //Control path
+    
+    input wire [7:0]cedp_descriptor_byte_i, //Descriptor manager
+    input wire cedp_descriptor_valid_i,
+   
+    input wire [2:0]STATE,
+
+    output reg [7:0]bmRequestType_o,  //Control path
+    output reg [7:0]bRequest_o,
+    output reg [15:0]wValue_o,
+    output reg [15:0]wIndex_o,
+    output reg [15:0]wLength_o
+    output reg cedp_descriptor_done_o
 );
+
+    always@(posedge cedp_clk_i or posedge cedp_reset)begin
+        if(cedp_reset)begin
+
+        end
+        else begin
+
+        end
+    end
 
 endmodule
