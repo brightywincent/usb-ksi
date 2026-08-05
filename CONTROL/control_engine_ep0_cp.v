bf2681f1fa1352fb4b467eeab75fec61708d9485
Brighty Wincent <brightywincent77@gmail.com>
2026-08-05 19:59:42 +0000

    Control Engine EP0 CP : Added progress.

--- CONTROL/control_engine_ep0_cp.v
+++ CONTROL/control_engine_ep0_cp.v
@@ -1,6 +1,7 @@
 `include "bRequest.vh"
 `include "DESCRIPTOR_TYPES.vh"
 `include "BASES.vh"
+//Max LUN = Number of LUNs − 1
 module control_engine_ep0_cp #(
     parameter STANDARD 2'b00,
     parameter CLASS 2'b01,
@@ -20,6 +21,8 @@ module control_engine_ep0_cp #(
     input wire [15:0]wIndex_i,
     input wire [15:0]wLength_i,
 
+    input wire cecp_halt_i, //EP halt flags ORed input 
+
     output reg descriptor_start_o, //Data path
     
     output reg cecp_ld_addr_o,  //Address manager
@@ -35,11 +38,25 @@ module control_engine_ep0_cp #(
     output reg [7:0]cecp_interface_o,
     
     output reg cecp_rd_curr_config_o,
-    output reg cecp_rd_interface_o
+    output reg cecp_rd_interface_o,
+
+    output reg cecp_ep1_out_halt_o,
+    output reg cecp_ep1_in_halt_o,
+
+    output reg cecp_ep1_out_clear_halt_o,
+    output reg cecp_ep1_in_clear_halt_o,
+
+    output reg [15:0] got_status,
+
+    output reg cecp_bot_reset_o,
+    output reg [7:0]cecp_max_lun_o
 );
 
     localparam IDLE = 3'd0;
     localparam EXECUTE = 3'd1;
+    localparam STATUS_IN = 3'd2;
+    localparam DATA_IN = 3'd3;
+    localparam STALL = 3'd4;
 
     reg [7:0]bmRequestType;
     reg [7:0]bRequest;
@@ -55,12 +72,20 @@ module control_engine_ep0_cp #(
             wIndex<=16'h00_00;
             wLength<=16'h00_00;
             STATE<=3'b0;
+            got_status<=16'h0000;
         end
         else begin
             cecp_rd_curr_config_o<=1'b0;
             cecp_set_config_o<=1'b0;
             cecp_rd_interface_o<=1'b0;
             cecp_ld_addr_o<=1'b0;
+            cecp_ep1_out_halt_o<=1'b0;
+            cecp_ep1_in_halt_o<=1'b0;
+            cecp_ep1_out_clear_halt_o<=1'b0;
+            cecp_ep1_in_clear_halt_o<=1'b0;
+            got_status<=16'h0000;
+            cecp_bot_reset_o<=1'b0;
+            cecp_max_lun_o<=8'h00;
             case(STATE)
                 IDLE : begin
                     if(cecp_setup_done_i) begin
@@ -76,13 +101,94 @@ module control_engine_ep0_cp #(
                     case(bmRequestType[6:5])
                         STANDARD : begin
                             case(bRequest)
-                                `GET_STATUS :
-                                `CLEAR_FEATURE :
-                                `SET_FEATURE :
+                                `GET_STATUS : begin
+                                        case(bmRequestType[4:0])
+                                            5'b0 : begin //Device
+                                                got_status<=16'h0000;
+                                                STATE<=DATA_IN;
+                                            end
+                                            5'b1 : begin //Interface
+                                                got_status<=16'h0000;
+                                                STATE<=DATA_IN;
+                                            end
+                                            5'b2 : begin //Endpoint
+                                                if(cecp_halt_i)begin
+                                                    got_status<=16'h01;
+                                                    STATE<=DATA_IN;
+                                                end
+                                                else 
+                                                    got_status<=16'h00;
+                                                    STATE<=DATA_IN;
+                                            end
+                                        endcase
+                                end
+                                `CLEAR_FEATURE : begin
+                                    case(bmRequestType[4:0])
+                                        5'b0 : begin //Device
+                                            case(wValue)       //Feature Selectors
+                                                //Device_Remote_Wakeup
+                                                16'h0001 : //STALL
+                                                STATE<=STALL;
+                                                //Test_Mode
+                                                16'h0002 : 
+                                                STATE<=STALL;//STALL 
+                                            endcase
+                                        end
+                                        5'b1 : begin //Interface
+                                            //No Feature selectors for STANDARD.
+                                            //STALL
+                                            STATE<=STALL;
+                                        end
+                                        5'b2 : begin //Endpoint
+                                            case(wIndex[7:0])
+                                                8'h01 : begin
+                                                    cecp_ep1_out_clear_halt_o<=1'b1;
+                                                    STATE<=STATUS_IN;
+                                                end
+                                                8'h81 : begin
+                                                    cecp_ep1_in_clear_halt_o<=1'b1;
+                                                    STATE<=STATUS_IN;
+                                                end
+                                            endcase
+                                        end
+                                    endcase
+                                end 
+                                `SET_FEATURE : begin
+                                    case(bmRequestType[4:0])
+                                        5'b0 : begin //Device
+                                            case(wValue)       //Feature Selectors
+                                                //Device_Remote_Wakeup
+                                                16'h0001 : //STALL
+                                                    STATE<=STALL;
+                                                //Test_Mode
+                                                16'h0002 : //STALL
+                                                    STATE<=STALL; 
+                                            endcase
+                                        end
+                                        5'b1 : begin //Interface
+                                            //No Feature selectors for STANDARD.
+                                            //STALL
+                                            STATE<=STALL;
+                                        end
+                                        5'b2 : begin //Endpoint
+                                            case(wIndex[7:0])
+                                                8'h01 : begin
+                                                    cecp_ep1_out_halt_o<=1'b1;
+                                                    STATE<=STATUS_IN;
+                                                end
+                                                8'h81 : begin
+                                                    cecp_ep1_in_halt_o<=1'b1;
+                                                    STATE<=STATUS_IN;
+                                                end
+                                            endcase
+                                        end
+                                    endcase
+                                end
                                 `SET_ADDRESS : begin
                                     cecp_new_addr_o<=wValue[6:0];
                                     cecp_ld_addr_o<=1'b1;
                                     //After status stage, give the commit signal to the address manager
+                                    STATE<=STATUS;
                                 end
                                 `GET_DESCRIPTOR : begin
                                     case(wValue[15:8])
@@ -90,12 +196,14 @@ module control_engine_ep0_cp #(
                                             if(wValue[7:0]==8'h00)begin
                                                 cecp_addr_o<=`DEVICE_BASE;
                                                 cecp_length_o<=(wLength < `DEVICE_LENGTH)?wLength:`DEVICE_LENGTH;
+                                                STATE<=DATA_IN;
                                             end
                                         end
                                         `TYPE_CONFIGURATION : begin
                                             if(wValue[7:0]==8'h00)begin
                                                 cecp_addr_o<=`CONFIGURATION_BASE;
                                                 cecp_length_o<=(wLength < `CONFIGURATION_LENGTH)?wLength:`CONFIGURATION_LENGTH;
+                                                STATE<=DATA_IN;
                                             end
                                         end
                                         `TYPE_STRING : begin
@@ -103,60 +211,83 @@ module control_engine_ep0_cp #(
                                                 8'h00 : begin
                                                     cecp_addr_o<=`STRING0_BASE;
                                                     cecp_length_o<=(wLength < `STRING0_LENGTH)?wLength:`STRING0_LENGTH;     
+                                                    STATE<=DATA_IN;
                                                 end
                                                 8'h01 : begin
                                                     cecp_addr_o<=`STRING1_BASE;
                                                     cecp_length_o<=(wLength < `STRING1_LENGTH)?wLength:`STRING1_LENGTH;     
+                                                    STATE<=DATA_IN;
                                                 end
                                                 8'h02 : begin
                                                     cecp_addr_o<=`STRING2_BASE;
                                                     cecp_length_o<=(wLength < `STRING2_LENGTH)?wLength:`STRING2_LENGTH;     
-                                                
+                                                    STATE<=DATA_IN;
                                                 end
                                                 8'h03 : begin
                                                     cecp_addr_o<=`STRING3_BASE;
                                                     cecp_length_o<=(wLength < `STRING3_LENGTH)?wLength:`STRING3_LENGTH;     
-                                                
+                                                    STATE<=DATA_IN;
                                                 end
                                             endcase
                                         end
-                                        STATE<=STATUS;
                                     endcase
                                 end
                                 `SET_DESCRIPTOR : begin
                                     //STALL for mass storage
+                                    STATE<=STALL;
                                 end
                                 `GET_CONFIGURATION : begin
                                     if(wLength==16'h0001)begin
                                         cecp_rd_curr_config_o<=1'b1;
-                                        STATE<=STATUS;
+                                        STATE<=DATA_IN;
                                     end
                                 end
                                 `SET_CONFIGURATION : begin
                                     if(wValue[7:0]<=8'h01)begin
                                         cecp_set_config_o<=1'b1;
                                         cecp_new_config_o<=wValue[7:0];
+                                        STATE<=STATUS_IN;
                                     end
                                     else //STALL
+                                        STATE<=STALL;
                                 end
                                 `GET_INTERFACE : begin
                                     if(wIndex=16'h0000) begin
                                         cecp_interface_o<=8'h00;
                                         cecp_rd_interface_o<=1'b1;
+                                        STATE<=DATA_IN;
                                     end
                                     else //STALL
+                                        STATE<=STALL;
                                 end
                                 `SET_INTERFACE : begin
                                     if(wIndex=16'h0000) begin
-                                        //ACK
+                                        STATE<=STATUS_IN;
                                     end
                                     else //STALL
+                                        STATE<=STALL;
                                 end
-                                `SYNCH_FRAME :
+                                `SYNCH_FRAME :begin
+                                    //For iscochronous endpoints
+                                    //STALL
+                                    STATE<=STALL;
+                                end
                             endcase
                         end
-                        CLASS : begin
-
+                        CLASS : begin  //this is for interface recipient
+                            case(bRequest)
+                                `BULK_ONLY_RESET : begin
+                                    if(wValue==16'h0000 && wLength==16'h0000)begin
+                                        cecp_bot_reset_o<=1'b1;
+                                        STATE<=STATUS_IN;
+                                    end
+                                end
+                                `GET_MAX_LUN : begin
+                                    if(wIndex==16'h0000 && wLength==16'h0001) begin
+                                        cecp_max_lun_o<=8'h00;
+                                    end
+                                end
+                            endcase
                         end
                         VENDOR : begin
 
@@ -166,11 +297,16 @@ module control_engine_ep0_cp #(
                         end
                     endcase
                 end
-                STATUS : begin
+                STATUS_IN : begin
+
+                end
+                DATA_IN : begin
+
+                end
+                STALL : begin
 
                 end
             endcase
         end
     end
-
 endmodule
